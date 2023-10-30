c4295738b8bda48265276d72ee924f950efff3ce
korn1evski <corn1evski@gmail.com>
2023-10-30 12:46:50 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+const http = require("http");
+
+const server = http.createServer(app)
+
+
+const _dirname = path.resolve(".")
+console.log(_dirname)
+const buildPath = path.join(_dirname, "../client/build")
+console.log(buildPath)
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+    res.sendFile(
+        _dirname + "/client/build/index.html",
+        function (err){
+            if(err){
+                res.status(500).send(err);
+            }
+        }
+    )
+})
+
+server.listen(5001 , () => console.log('Listening to port 5001'))
